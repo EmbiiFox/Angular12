1ca11cfdba5ad7ff0c8abd4f7156e27ad97349e2
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2021-12-14 16:28:37 +0700

    directive

---
+++ src/app/Directives/highlight.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+  @Input() appHighlight='pink';
+  constructor(private el: ElementRef) { 
+    console.log('contructor HighlightDirective',this.appHighlight);
+    // el.nativeElement.style.color = this.appHighlight;
+  }
+  ngOnInit(): void {
+    console.log('HighlightDirective',this.appHighlight);
+    this.el.nativeElement.style.color = this.appHighlight;
+  }
+}
