fa378fe2f22d33fe63c63707f460fa1b5e5f4253
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2021-12-15 17:54:48 +0700

    http POST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,11 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { HighlightDirective } from './Directives/highlight.directive';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
-
+import { GetDataComponent } from './get-data/get-data.component';
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, OnSalePipe, PageNotFoundComponent, HighlightDirective, TemplateDrivenFormComponent, ReactiveFormComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  declarations: [AppComponent, HomeComponent, AboutComponent, OnSalePipe, PageNotFoundComponent, HighlightDirective, TemplateDrivenFormComponent, ReactiveFormComponent, GetDataComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/get-data/get-data.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpServerService} from '../Services/https-server.service';
+
+@Component({
+  selector: 'app-get-data',
+  templateUrl: './get-data.component.html',
+  styleUrls: ['./get-data.component.scss']
+})
+export class GetDataComponent implements OnInit {
+
+  constructor(private httpServerService: HttpServerService) { }
+
+  public ngOnInit(): void {
+    this.httpServerService.getComments().subscribe(data=> {
+      console.log('data', data);
+    });
+    this.httpServerService.getRandomUsers().subscribe(data=> {
+      console.log('get random users', data.results);
+    })
+  }
+
+}
